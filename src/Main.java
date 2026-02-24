466e788e6b21ce823ca42307a9fc1d1e4c36fc57
fatigued-orca <81499130+fatigued-orca@users.noreply.github.com>
2026-02-23 21:28:55 -0800

    changed to work with pokershuffler file
    Replaced console output with GUI initialization.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-public class Main {
-    public static void main (String[] args)
-    {
-        System.out.println("Hello");
-        System.out.println("Test");
-    }
-}
+import javax.swing.*;
 
+public class Main {
+    public static void main(String[] args) {
+
+            SwingUtilities.invokeLater(PokerShuffler::new); //run GUI
+        }
+    }
